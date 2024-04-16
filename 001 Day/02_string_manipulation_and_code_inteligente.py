8a36fce95efbc4a21ff04c58d5f184eb76c90eab
naszi <naszi75@gmail.com>
2024-04-16 20:29:05 +0200

    Added some string manipulation

---
+++ 001 Day/02_string_manipulation_and_code_inteligente.py
@@ -0,0 +1,4 @@
+print("Hello World!")  # single line
+print("Hello World!\nHello World!\nHello World!")  # \n -> new line operator
+print("Hello " + "Szabolcs")  # + -> the two strings are connected
+
