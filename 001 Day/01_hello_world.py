b12387e9c5290073a347abcc29ed16c8d06e0597
naszi <naszi75@gmail.com>
2024-04-16 20:08:44 +0200

    Added some printing function

--- 001 Day/01_hello_world.py
+++ 001 Day/01_hello_world.py
@@ -1,2 +1,15 @@
 print("Hello World!")
 print('My name\'s Szabolcs.')
+
+print("A 'single quote' inside a double quote")
+print('A "double quote" inside a single quote')
+print("Alternatively you can just \"escape\" the quote")
+
+print("She said: \"Hello\" and then left.")
+print('She said: "Hello" and then left.')
+
+print("1. Mix 500g of Flour, 10g Yeast and 300ml Water in a bowl.")
+print("2. Knead the dough for 10 minutes.")
+print("3. Add 3g of Salt.")
+print("4. Leave to rise for 2 hours.")
+print("5. Bake at 200 degrees C for 30 minutes.")
